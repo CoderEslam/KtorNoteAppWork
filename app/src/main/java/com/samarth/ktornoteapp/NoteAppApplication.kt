9001e5cc3af48767f61756325747c160b54ed059
eslam <eslamfgazy600@gmail.com>
2022-08-06 20:49:46 +0200

    Ktor Working

---
+++ app/src/main/java/com/samarth/ktornoteapp/NoteAppApplication.kt
@@ -0,0 +1,9 @@
+package com.samarth.ktornoteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteAppApplication: Application(){
+
+}
---
+++ app/src/main/java/com/samarth/ktornoteapp/repository/NoteRepo.kt
@@ -0,0 +1,24 @@
+package com.samarth.ktornoteapp.repository
+
+import com.samarth.ktornoteapp.data.local.models.LocalNote
+import com.samarth.ktornoteapp.data.remote.models.User
+import com.samarth.ktornoteapp.utils.Result
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepo {
+
+    suspend fun createUser(user:User):Result<String>
+    suspend fun login(user:User):Result<String>
+    suspend fun getUser():Result<User>
+    suspend fun logout():Result<String>
+
+
+    suspend fun createNote(note:LocalNote): Result<String>
+    suspend fun updateNote(note:LocalNote): Result<String>
+    fun getAllNotes():Flow<List<LocalNote>>
+    suspend fun getAllNotesFromServer()
+
+
+    suspend fun deleteNote(noteId:String)
+    suspend fun syncNotes()
+}
